7304916fabcba2e256c6b13b02a1dec962050bf3
preston176 <prestonnyamweya@gmail.com>
2024-02-15 22:11:01 +0300

    Update font weight for Trusted By heading

--- src/Components/TrustedBy/TrustedBy.jsx
+++ src/Components/TrustedBy/TrustedBy.jsx
@@ -4,7 +4,7 @@ import ImageSlider from '../ImageSlider/ImageSlider'
 const TrustedBy = () => {
     return (
         <div className='py-4'>
-            <h3 className='text-center text-[40px]'>Trusted By ...</h3>
+            <h3 className='text-center text-[40px] font-bold'>Trusted By ...</h3>
             <ImageSlider />
         </div>
     )
