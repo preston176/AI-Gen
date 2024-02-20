0e7b2b8c6484bdebd9cdb3adb5a2a98776077f9d
preston176 <prestonnyamweya@gmail.com>
2024-02-20 21:10:15 +0300

    Add default image and link to generate image page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ImageSlider from '../ImageSlider/ImageSlider'
+import defaultImage from '../../assets/defaultImage.jpg'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
     return (
@@ -7,9 +9,12 @@ const Hero = () => {
             <div className='mb-4 mt-8 flex flex-col justify-evenly items-center h-[40vh] bg-[#858585] bg-opacity-10 shadow-gray-700 rounded-3xl]'>
 
                 <h2 className='bg-gradient-to-r from-red-500 to-blue-500 text-transparent bg-clip-text laptop:text-[60px] font-mono p-4 md:text-[76px] sm:text-[46px] mobile:text-[28px] small_mobile:text-[25px] text-center' >Create Stunning Images with AI</h2>
+
+
+
                 <div className="">
 
-                    <h3 className='btn'>Get Started</h3>
+                    <Link to="/generate_image">  <h3 className='btn'>Get Started</h3></Link>
 
                 </div>
             </div>
