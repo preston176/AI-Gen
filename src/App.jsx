60029bce2d8dbc8177342bc725e0f1ca3e553c24
preston176 <prestonnyamweya@gmail.com>
2024-02-20 21:10:24 +0300

    Add header, footer, and about page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import ImageGen from './Pages/ImageGen';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import About from './Pages/About';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/generate_image" element={<ImageGen />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes>
+        <div className='mx-auto max-w-7xl'>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/generate_image" element={<ImageGen />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Routes>
+          <Footer />
+        </div>
       </BrowserRouter>
     </>
   )
