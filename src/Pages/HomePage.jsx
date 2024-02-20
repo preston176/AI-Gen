5f24c744702e1d2a07b2f7c830640813c5476914
preston176 <prestonnyamweya@gmail.com>
2024-02-20 21:10:06 +0300

    Remove unused components from HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import Header from '../Components/Header/Header'
 import Hero from '../Components/Hero/Hero'
 import Mid from '../Components/Mid/Mid'
-import TrustedBy from '../Components/TrustedBy/TrustedBy'
 import Testimonial from '../Components/Testimonial/Testimonial'
 
 const HomePage = () => {
     return (
-        <div className='mx-auto max-w-7xl'>
-            <Header />
+
+        <>
             <Hero />
             <Mid />
-            <TrustedBy />
             <Testimonial />
-        </div>
+        </>
+
     )
 }
 
