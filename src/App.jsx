8be7e97670f071b8ae733ea2f5a00c44585fef09
preston176 <prestonnyamweya@gmail.com>
2024-05-29 20:54:04 +0300

    refactor: Remove unused import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
